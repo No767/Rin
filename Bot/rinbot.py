41444c632c57b50de576e8ea7cc5d20b9c1404db
No767 <73260931+No767@users.noreply.github.com>
2022-06-18 15:23:37 -0700

    correct my own mistakes

--- Bot/rinbot.py
+++ Bot/rinbot.py
@@ -15,36 +15,36 @@ logging.basicConfig(
     datefmt="[%m/%d/%Y] [%I:%M:%S %p %Z]",
 )
 
-TOKEN = os.getenv("Rin_Prod_Testing_Key")
+TOKEN = os.getenv("TOKEN")
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix=".", help_command=None, intents=intents)
 
 # Loads in all extensions
 initial_extensions = [
-    # "Cogs.rininfo",
-    # "Cogs.rinping",
-    # "Cogs.rinhelp",
+    "Cogs.rininfo",
+    "Cogs.rinping",
+    "Cogs.rinhelp",
     "Cogs.reddit",
-    # "Cogs.mcsrvstats",
-    # "Cogs.waifu-generator",
-    # "Cogs.waifu-pics",
-    # "Cogs.advice",
-    # "Cogs.jikan",
-    # "Cogs.global-error-handling",
-    # "Cogs.rininvite",
-    # "Cogs.version",
-    # "Cogs.youtube",
-    # "Cogs.tenor",
-    # "Cogs.uptime",
-    # "Cogs.mangadex",
-    # "Cogs.bot-info",
-    # "Cogs.help",
-    # "Cogs.modrinth",
-    # "Cogs.discord-bots",
-    # "Cogs.legacy-help",
-    # "Cogs.jisho",
-    # "Cogs.anilist"
+    "Cogs.mcsrvstats",
+    "Cogs.waifu-generator",
+    "Cogs.waifu-pics",
+    "Cogs.advice",
+    "Cogs.jikan",
+    "Cogs.global-error-handling",
+    "Cogs.rininvite",
+    "Cogs.version",
+    "Cogs.youtube",
+    "Cogs.tenor",
+    "Cogs.uptime",
+    "Cogs.mangadex",
+    "Cogs.bot-info",
+    "Cogs.help",
+    "Cogs.modrinth",
+    "Cogs.discord-bots",
+    "Cogs.legacy-help",
+    "Cogs.jisho",
+    "Cogs.anilist"
 ]
 for extension in initial_extensions:
     bot.load_extension(extension)
